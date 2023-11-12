a3ee67cc710015bb74c69509bde8d5715f96a17c
John Riordan <iam.john.riordan@gmail.com>
2023-11-11 17:59:59 -0800

    feat: misc

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
-import Image from 'next/image';
+import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Link href='/na1/rió'>Rió profile</Link>
+    </main>
+  );
 }
